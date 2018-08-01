0ca58a5fd647417188be94351dc00ee0071a45b7
Thomas Jc <thomas.jcng@gmail.com>
2018-08-01 13:56:12 +0800

    Remove no longer needed shell script.

--- start_mongo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# to start a Mongo container during development stage
-docker run --rm -it -dp 27017:27017 \
-	--name some-mongo \
-	-e MONGO_INITDB_ROOT_USERNAME=admin \
-	-e MONGO_INITDB_ROOT_PASSWORD=example \
-	mongo
-
